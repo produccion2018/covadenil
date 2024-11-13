5361fc9850fc015fe4409fb61979693a4189c404
mauro pinto <mauroalexanderpinto@gmail.com>
2024-11-13 14:17:33 -0300

    actualizacion de pagina web

--- js/main.js
+++ js/main.js
@@ -125,3 +125,6 @@ function actualizarNumerito() {
     let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
     numerito.innerText = nuevoNumerito;
 }
+
+
+
